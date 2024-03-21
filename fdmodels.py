1c5800bac1b3b97eead61b58032a4593e72a6641
mayarakok <akokmayar607@gmail.com>
2024-03-21 14:28:16 +0300

    Seeded the drivers data in the database

--- fdmodels.py
+++ fdmodels.py
@@ -1,8 +1,18 @@
-from sqlalchemy.orm import declarative_base,sessionmaker
-from sqlalchemy import create_engine,Column,Integer,String
+from sqlalchemy.orm import declarative_base,sessionmaker,relationship
+from sqlalchemy import create_engine,Column,Integer,String,ForeignKey,DateTime,Float,Table,func
 
 
 Base = declarative_base()
+
+client_driver = Table(
+    "client_drivers",
+    Base.metadata,
+    
+    Column('client_id',ForeignKey('clients.id'),primary_key=True),
+    Column("driver_id",ForeignKey('drivers.id'),primary_key=True),
+    extend_existing= True
+    
+)
 
 class Client(Base):
     __tablename__ = "clients"
@@ -12,19 +22,16 @@ class Client(Base):
     phone = Column(Integer())
     location = Column(String())
     
+    # one-to-many relationship(one client has many trips)
+    trips = relationship('Trip',backref='client')
+    
+    # many-to-many relations between the clients and drivers
+    drivers = relationship('Driver',secondary=client_driver, back_populates='clients')
+    
     def __repr__(self):
         return f"<Client(name={self.name}, phone={self.phone}, location={self.location})>"
     
-    # @property
-    # def phone(self):
-    #     return self._phone 
-    
-    # @phone.setter
-    # def phone(self,phone):
-    #     if isinstance(phone,str) and len(str(phone)) == 10:
-    #         self._phone = phone 
-    #     else:
-    #         return "The phone number should be 10 digits and should start with 07 or 01"    
+      
     
 class Driver(Base):
     __tablename__ = "drivers"
@@ -32,11 +39,42 @@ class Driver(Base):
     id = Column(Integer(),primary_key=True)
     name = Column(String())
     phone = Column(Integer())
-    location = Column(Integer())
+    location = Column(String())
     
+    # one-to-many relationship(one driver has many trips)
+    trips = relationship('Trip',backref='driver')
+    
+    # many-to-many relationship between the drivers and clients
+    clients = relationship("Client",secondary=client_driver, back_populates="drivers")
     
     def __repr__(self):   
         return f"<Client(name={self.name}, phone={self.phone}, location={self.location})>"
+    
+class Trip(Base):
+    
+    __tablename__ = "trips"
+    
+    id = Column(Integer(),primary_key=True)
+    customer_id = Column(Integer(), ForeignKey('clients.id'))
+    driver_id = Column(Integer(),ForeignKey("drivers.id"))
+    start_location = Column(String, nullable=False)
+    end_location = Column(String, nullable=False)
+    start_time = Column(DateTime(), server_default=func.now())
+    end_time = Column(DateTime(), onupdate=func.now())
+    Price = Column(Float)
+    
+    def __repr__(self):
+        return f"<Trip(start_location={self.start_location}, end_location={self.end_location}, fare={self.price})>"
+
+    @property
+    def duration(self):
+        if self.end_time:
+            return (self.end_time - self.start_time).seconds / 60  # Duration in minutes
+        else:
+            return None
+    
+    
+        
     
     
     
--- seed.py
+++ seed.py
@@ -1,7 +1,43 @@
-from fdmodels import create_engine,Base,sessionmaker
+from fdmodels import create_engine,Base,sessionmaker,Driver
+from faker import Faker
 
 engine = create_engine("sqlite:///fastdrive.db")
 Base.metadata.create_all(engine)
     
 session = sessionmaker(bind=engine)
 mysession = session()
+
+fakedata = Faker()
+
+print("Seeding drivers")
+
+# clearing data before seeding
+mysession.query(Driver).delete()
+
+location = ["Westlands",
+    "Kasarani",
+    "Embakasi East",
+    "Embakasi West",
+    "Embakasi Central",
+    "Embakasi North",
+    "Embakasi South",
+    "Starehe",
+    "Lang'ata",
+    "Kibra",
+    "Dagoretti North",
+    "Dagoretti South",
+    "Mathare",
+    "Roysambu",
+    "Ruaraka",
+    "Makadara"]
+
+for i in range(30):
+    driver = Driver(name=fakedata.name(),phone = fakedata.phone_number(), location=fakedata.random_element(location))
+    mysession.add(driver)
+    mysession.commit()
+    
+print("All drivers have been seeded")    
+    
+    
+    
+
